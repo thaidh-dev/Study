6106bb9c42f0be46a978dc1abe53f5da557ae9c3
Dương Thái <duonghongthai07@gmail.com>
2021-09-07 08:12:02 +0700

    07-09-2021 08:12:02

---
+++ Downloads/Flutter/flutter_springBoot_postgresql/todoey_flutter/lib/widgets/tasks_list.dart
@@ -0,0 +1,33 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+import 'package:todoey_flutter/widgets/task_tile.dart';
+
+class TaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              isChecked: task.isDone,
+              taskTitle: task.name,
+              checkboxCallback: () {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
