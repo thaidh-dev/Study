3a62191d5b148aa7e4997d6c7aadc82bd93e5cd6
Dương Thái <duonghongthai07@gmail.com>
2021-12-19 09:59:25 +0700

    19-12-2021 09:59:25

--- Brain/Captcha/demo.js
+++ Brain/Captcha/demo.js
@@ -3,12 +3,12 @@ const puppeteer = require('puppeteer');
 const edgeOptions = {
   executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
   headless: false,
-//   slowMo: 10,
+  // slowMo: 10,
   defaultViewport: null
 };
 
 (async () => {
-//   const browser = await puppeteer.launch(edgeOptions);
+  // const browser = await puppeteer.launch(edgeOptions);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.google.com/recaptcha/api2/demo');
@@ -18,6 +18,8 @@ const edgeOptions = {
   let elementHandle = await page.$("div#recaptcha-demo iframe");
   let iframe = await elementHandle.contentFrame();
   // now iframe is like "page", and to click in the button you can do this
+  // await iframe.click("#recaptcha-anchor > div.recaptcha-checkbox-border");
+  //or
   await iframe.evaluate(() => {
     document.querySelector("#recaptcha-anchor > div.recaptcha-checkbox-border").click();
   });
