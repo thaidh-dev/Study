34653d1f31fddf46439f1fc81ec07d90d6d6a4a6
Dương Thái <duonghongthai07@gmail.com>
2022-02-06 21:32:33 +0700

    06-02-2022 21:32:33

--- Brain/Captcha/demo.js
+++ Brain/Captcha/demo.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer');
 
+const extensionPath = 'C:\\Users\\Admin\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Extensions\\admkpobhocmdideidcndkfaeffadipkc\\1.3.1_0'
 const edgeOptions = {
   executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
-  headless: false,
-  // slowMo: 10,
-  defaultViewport: null
+  headless: true, // quyết định có mở browser lên cho thấy hay ko
+  slowMo: 10,
+  defaultViewport: null,
+  args: [
+    `--disable-extensions-except=${extensionPath}`,
+    `--load-extension=${extensionPath}`,
+  ]
 };
 
 (async () => {
-  // const browser = await puppeteer.launch(edgeOptions);
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(edgeOptions);
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.google.com/recaptcha/api2/demo');
 
@@ -23,6 +28,13 @@ const edgeOptions = {
   await iframe.evaluate(() => {
     document.querySelector("#recaptcha-anchor > div.recaptcha-checkbox-border").click();
   });
+
+  // pass captcha
+  await page.waitForTimeout(3000);
+  await page.waitForSelector("iframe");
+  const captchaIframe = await (await page.$('iframe[src="https://www.google.com/recaptcha/api2/bframe?hl=en&v=1p3YWy80wlZ7Q8QFR1gjazwU&k=6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"]')).contentFrame();
+  await captchaIframe.click('div.help-button-holder');
+  await page.waitForTimeout(10000);
 
   // submit
   let submit = await page.$('input#recaptcha-demo-submit');
